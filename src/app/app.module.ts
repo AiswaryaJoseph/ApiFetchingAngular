f3915f64370b560618b3f64d35184108b82fa82f
AiswaryaJoseph <celinaish2021@gmail.com>
2023-06-02 07:09:08 +0530

    apiAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDespComponent } from './product-desp/product-desp.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import {HttpClientModule} from '@angular/common/http'
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDespComponent,
+    NavbarComponent,
+    FooterComponent,
+   
+   
+   
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-desp/product-desp.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-product-desp',
+  templateUrl: './product-desp.component.html',
+  styleUrls: ['./product-desp.component.css']
+})
+export class ProductDespComponent {
+  result:any
+constructor(private http:HttpClient){
+  this.result=[];
+}
+
+
+ngOnInit():void{
+  this.getAllProducts()
+}
+
+
+getAllProducts(){
+  return this.http.get("https://dummyjson.com/products").subscribe((data:any) =>{
+    console.log(data);
+    this.result=data;
+  })
+}
+
+}
